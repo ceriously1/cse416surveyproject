8326e4b13a88eb00f741362f45327081207eabd2
ceriously1 <66987243+ceriously1@users.noreply.github.com>
2022-04-14 04:56:05 -0400

    Removed some deprecated comments

--- uwu/api/models/user.js
+++ uwu/api/models/user.js
@@ -13,23 +13,3 @@ User.plugin(passportLocalMongoose);
 
 // note that the 3rd argument is the specific db name
 module.exports = mongoose.model('User', User, 'Users');
-
-
-
-///////////// youtube tutorial implemention
-// const mongoose = require('mongoose');
-
-// const userSchema = mongoose.Schema({
-//     _id: mongoose.Schema.Types.ObjectId,
-//     username: { type: String, required: true, unique: true },
-//     // we'll need to implement email verification later
-//     email: { type: String, required: true, unique: true },
-//     password: { type: String, required: true },
-//     balance: { type: Number, required: true},
-//     profile_info: {pfp_path: String, stat1: Number, stat2: Number},
-//     completed_survey: [mongoose.Types.ObjectId],
-//     failed_survey: [mongoose.Types.ObjectId],
-//     in_progress_survey: [mongoose.Types.ObjectId]
-// }, {versionKey: false});
-
-// module.exports = mongoose.model('User', userSchema, 'Users');
